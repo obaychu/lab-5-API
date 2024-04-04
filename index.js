b97b2ea4afc29748481699c77efa6db1e31b7f85
obaychu <olivia.baychu@edu.sait.ca>
2024-04-03 22:11:57 -0600

    remove extra Fetch

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-// fetch = Function used for making HTTP requests to fetch resources.
-//         (JSON style data, images, files)
-//         Simplifies asynchronous data fetching in JavaScript and
-//         used for interacting with APIs to retrieve and send
-//         data asynchronously over the web.
-//         fetch(url, {options})
 
 async function fetchData(){
 
